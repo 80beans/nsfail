a82c19a9386ce6f7040062de2c5b0f55a4211b7e
Robert Beekman <robert@matsimitsu.nl>
2011-10-27 20:16:23 +0200

    Working

--- lib/fail.rb
+++ lib/fail.rb
@@ -4,4 +4,22 @@ class Fail
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :created_at
+  field :from_user
+  field :from_user_id
+  field :geo, :type => Hash, :default => {}
+  field :iso_language_code
+  field :medatada, :type => Hash, :default => {}
+  field :profile_image_url
+  field :source
+  field :text
+
+  def self.create_from_tweet(tweet)
+    new_fail = {}
+    self.fields.each do |fld, metadata|
+      new_fail[fld] = tweet[fld]
+    end
+    create!(new_fail)
+  end
+
 end
